92274692188e71a9b3e251dce08cc24e01887903
Krzysztof Chojnowski <krzysztof.chojnowski@somlabs.com>
2022-02-16 17:44:23 +0100

    SOMLABS: allow using UART4 by application core

--- plat/imx/imx8m/imx8mm/platform.mk
+++ plat/imx/imx8m/imx8mm/platform.mk
@@ -153,7 +153,7 @@ $(eval $(call add_define,BL32_BASE))
 BL32_SIZE		?=	0x2000000
 $(eval $(call add_define,BL32_SIZE))
 
-IMX_BOOT_UART_BASE	?=	0x30890000
+IMX_BOOT_UART_BASE	?=	0x30a60000
 $(eval $(call add_define,IMX_BOOT_UART_BASE))
 
 $(eval $(call add_define,IMX8M_DDR4_DVFS))
